5fefb98e3ad358922f8420f94f2cc17d7ff4639f
Yuri Andriaccio <yurand2000@gmail.com>
2025-09-18 13:45:57 +0200

    MPR first implementation

---
+++ src/analyses/multiprocessor_resource_model/README.md
@@ -0,0 +1,23 @@
+# Multiprocessor Periodic Resource Model
+
+[**🏠 Go back to all analyses**](../../../README.md#-available-analyses)
+
+🚧 Under Construction 🚧
+
+### 🔑 Preconditions
+
+---
+
+- Tasks are deadline constrained and sporadic.
+- Each processor on the platform has the same computing capacity.
+- Any job can be preempted and migrated on any core.
+
+### 🧪 Implemented Analyses
+
+---
+
+- **Joseph & Pandya** (1986) [^1]
+
+### 📚 References
+
+[^1]: M. Joseph and P. Pandya, “Finding Response Times in a Real-Time System,” Comput J, vol. 29, no. 5, pp. 390–395, 1986, doi: 10.1093/comjnl/29.5.390.
---
+++ src/analyses/multiprocessor_resource_model/mod.rs
@@ -0,0 +1,271 @@
+use crate::prelude::*;
+
+#[derive(Clone)]
+#[derive(Debug)]
+pub enum Error {
+    
+}
+
+pub struct MPRModel {
+    pub resource: Time,
+    pub period: Time,
+    pub concurrency: i64,
+}
+
+// -----------------------------------------------------------------------------
+
+impl MPRModel {
+    pub fn is_feasible(&self) -> bool {
+        self.resource <= self.concurrency * self.period
+    }
+
+    pub fn utilization(&self) -> f64 {
+        self.resource.as_raw() as f64 / self.period.as_raw() as f64
+    }
+
+    // Equation 1 [1]
+    pub fn supply_bound_function(&self, time: Time) -> Time {
+        #[inline(always)]
+        fn k(model: &MPRModel, time: Time) -> i64 {
+            Time::div_floor_time(
+                time - model.period + Time::div_ceil_i64(model.resource, model.concurrency),
+                model.period
+            )
+        }
+
+        #[allow(non_snake_case)]
+        #[inline(always)]
+        fn I(model: &MPRModel, time: Time) -> Time {
+            time - 2 * model.period + Time::div_ceil_i64(model.resource, model.concurrency)
+        }
+
+        // sbf conditions
+        if time >= self.period - Time::div_ceil_i64(self.resource, self.concurrency) {
+            k(self, time) * self.resource + Time::max(
+                Time::zero(),
+                (I(self, time) - k(self, time) * self.period) * self.concurrency + self.resource
+            )
+        } else {
+            Time::zero()
+        }
+    }
+}
+
+
+// global EDF for MPR ----------------------------------------------------------
+// Equation 2 [1]
+pub fn linear_supply_bound_function(interval: Time, period: Time, resource: Time, concurrency: i64) -> Time {
+    resource.as_nanos() * (interval - 2 * (period - resource / concurrency)) / period.as_nanos()
+
+    // Time::nanos_f64(resource.as_nanos_f64() / period.as_nanos_f64() * (interval.as_nanos_f64() - 2.0 * (period.as_nanos_f64() - resource.as_nanos_f64() / concurrency as f64))).floor()
+}
+
+pub fn linear_supply_bound_function_inverse(lsbf: Time, interval: Time, period: Time, concurrency: i64) -> Time {
+    let a = 2.0 / concurrency as f64;
+    let b = interval.as_nanos_f64() - 2.0 * period.as_nanos_f64();
+    let c = - period.as_nanos_f64() * lsbf.as_nanos_f64();
+
+    let negb = -b;
+    let bsqr = b * b;
+
+    let neg4ac = -4.0 * a * c;
+
+    let pos2a = 2.0 * a;
+
+    Time::nanos_f64((negb - f64::sqrt(bsqr + neg4ac)) / pos2a)
+}
+
+// Equation 3 [1]
+fn workload_upperbound(task: &RTTask, time: Time) -> Time {
+    activations_in_interval(task, time) * task.wcet + carry_in(task, time)
+}
+
+// Equation 3 [1]
+fn activations_in_interval(task: &RTTask, time: Time) -> i64 {
+    Time::div_floor_time(time + task.period - task.deadline, task.period)
+}
+
+// Equation 3 [1]
+fn carry_in(task: &RTTask, time: Time) -> Time {
+    Time::min(task.wcet, Time::max(Time::zero(), time - activations_in_interval(task, time) * task.period))
+}
+
+// Theorem 1, Equation 5 [1]
+fn demand(taskset: &[RTTask], k: usize, release: Time, num_processors: i64) -> Time {
+    let interference_hat = |taskset: &[RTTask], i: usize| -> Time {
+        let task_i = &taskset[i];
+        let task_k = &taskset[k];
+
+        if i != k {
+            Time::min(
+                workload_upperbound(task_i, release + task_k.deadline)
+                    - carry_in(task_i, release + task_k.deadline),
+                release + task_k.deadline - task_k.wcet
+            )
+        } else {
+            Time::min(
+                workload_upperbound(task_k, release + task_k.deadline)
+                    - task_k.wcet
+                    - carry_in(task_k, release + task_k.deadline),
+                release
+            )
+        }
+    };
+
+    let interference_flat = |taskset: &[RTTask], i: usize| -> Time {
+        let task_i = &taskset[i];
+        let task_k = &taskset[k];
+
+        if i != k {
+            Time::min(
+                workload_upperbound(task_i, release + task_k.deadline),
+                release + task_k.deadline - task_k.wcet
+            )
+        } else {
+            Time::min(
+                workload_upperbound(task_k, release + task_k.deadline) - task_k.wcet,
+                release
+            )
+        }
+    };
+
+    let sum0 = (0..taskset.len())
+        .map(|i| interference_hat(taskset, i))
+        .sum::<Time>();
+
+    // let mut vec1: Vec<_> = (0..taskset.len())
+    //     .map(|i| interference_flat(taskset, i) - interference_hat(taskset, i))
+    //     .collect();
+
+    // vec1.sort_unstable();
+
+    // let sum1 = vec1.into_iter().rev()
+    //     .take(num_processors as usize - 1)
+    //     .sum();
+
+    let mut vec1: Vec<_> = (0..taskset.len())
+        .map(|i| (i, carry_in(&taskset[i], release + taskset[k].deadline)))
+        .collect();
+
+    vec1.sort_unstable_by_key(|(_, ci)| *ci);
+    
+    // get the num_processors - 1 carry_ins and compute interferences for those
+    let sum1 = vec1.into_iter().rev()
+        .take(num_processors as usize - 1)
+        .map(|(i, _)| interference_flat(taskset, i) - interference_hat(taskset, i))
+        .sum();
+   
+    sum0 + sum1 + num_processors * taskset[k].wcet
+}
+
+// -----------------------------------------------------------------------------
+
+pub fn generate_interface_global_edf(taskset: &[RTTask], period: Time) -> MPRModel {
+    let v : Vec<_> = (
+        num_processors_lower_bound(taskset) ..=
+        num_processors_upper_bound(taskset)
+    ).collect();
+
+    let num_processors = 
+        v.binary_search_by(|i: &i64| -> std::cmp::Ordering {
+            todo!()
+        })
+        .unwrap() as i64; // given the upperbound, this cannot fail
+
+    todo!()
+}
+
+fn num_processors_lower_bound(taskset: &[RTTask]) -> i64 {
+    f64::ceil(RTUtils::total_utilization(taskset)) as i64
+}
+
+fn num_processors_upper_bound(taskset: &[RTTask]) -> i64 {
+    assert!(!taskset.is_empty());
+
+    let n = taskset.len() as i64;
+
+    let total_work: Time = taskset.iter()
+        .map(|task| task.wcet)
+        .sum();
+
+    let den = taskset.iter()
+        .map(|task| task.deadline - task.wcet)
+        .min()
+        .unwrap();
+
+    total_work / den + n
+}
+
+// -----------------------------------------------------------------------------
+// Convert each MPRModel to a set of periodic tasks (with implicit deadline)
+// that represent the high-level requirements for the scheduled taskset. This
+// set of server tasks can be scheduled with uniprocessor algorithms, as they
+// are meant to be pinned to invididual CPUs.
+
+impl Into<Vec<RTTask>> for MPRModel {
+    fn into(self) -> Vec<RTTask> {
+        #[inline(always)]
+        fn psi(model: &MPRModel) -> Time {
+            model.resource - model.concurrency * Time::div_floor_i64(model.resource, model.concurrency)
+        }
+
+        let k = psi(&self).as_nanos();
+
+        (0..self.concurrency)
+            .map(|i| {
+                let wcet =
+                    if i <= k {
+                        Time::div_floor_i64(self.resource, self.concurrency) + Time::one()
+                    } else if i == k + 1 {
+                        Time::div_floor_i64(self.resource, self.concurrency)
+                            + psi(&self) - k * Time::div_floor_i64(psi(&self), k)
+                    } else {
+                        Time::div_floor_i64(self.resource, self.concurrency)
+                    };
+
+                RTTask {
+                    wcet: wcet,
+                    deadline: self.period,
+                    period: self.period,
+                }
+            })
+            .collect()
+    }
+}
+
+impl Into<(RTTask, i64)> for MPRModel {
+    fn into(self) -> (RTTask, i64) {
+        let task =
+            RTTask {
+                wcet: Time::div_floor_i64(self.resource, self.concurrency) + Time::one(),
+                deadline: self.period,
+                period: self.period,
+            };
+
+        (task, self.concurrency)
+    }
+}
+
+// Tests -----------------------------------------------------------------------
+#[test]
+fn test_lsbf() {
+    let resource = Time::millis(50);
+    let period = Time::millis(300);
+    let concurrency = 3;
+    let interval = Time::millis(10);
+
+    // let resource = Time::raw(100);
+    // let period = Time::raw(100);
+    // let concurrency = 1;
+    // let interval = Time::raw(100);
+
+    let lsbf = linear_supply_bound_function(interval, period, resource, concurrency);
+    let inverse = linear_supply_bound_function_inverse(lsbf, interval, period, concurrency);
+
+    assert_eq!(resource.as_nanos(), inverse.as_nanos());
+}
+
+// References ------------------------------------------------------------------
+// [1] I. Shin, A. Easwaran, and I. Lee, “Hierarchical Scheduling Framework for
+//     Virtual Clustering of Multiprocessors,” in 2008 Euromicro Conference on
+//     Real-Time Systems, July 2008, pp. 181–190. doi: 10.1109/ECRTS.2008.28.
